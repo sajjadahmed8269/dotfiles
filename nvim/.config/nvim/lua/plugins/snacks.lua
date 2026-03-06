f2241ee040af1bc77c924d05da0edb292da6e227
Sajjad Ahmed <sajjadahmed8269@gmail.com>
2026-03-06 22:34:38 +0500

    Neovim snacks fix

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,8 +1,19 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    explorer = {
+      replace_netrw = true,
+    },
     picker = {
       sources = {
+        explorer = {
+          hidden = true,
+          actions = {
+            -- make l/h/CR expand and collapse directories
+            ["l"] = { action = "confirm" },
+            ["h"] = { action = "explorer_close" },
+          },
+        },
         files = {
           hidden = true,
         },
